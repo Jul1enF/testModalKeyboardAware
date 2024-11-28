c2578b4de36ee8fdd703df680ef092d98ae616f6
Jul1enF <j.furic@gmail.com>
2024-11-28 14:39:47 +0100

    min reproducible example

--- App.js
+++ App.js
@@ -1,18 +1,10 @@
 import { View, TextInput, Text, StyleSheet, KeyboardAvoidingView, ScrollView, Platform, TouchableOpacity } from "react-native";
 import { RPH, RPW } from "./modules/dimensions"
-
-import FontAwesome from 'react-native-vector-icons/FontAwesome';
-
 import { LinearGradient } from "expo-linear-gradient";
-
 import { useState, useEffect, useRef } from "react";
-
 import Modal from "react-native-modal"
-
 import { KeyboardProvider } from "react-native-keyboard-controller";
 import { KeyboardAwareScrollView, KeyboardToolbar } from "react-native-keyboard-controller";
-
-
 
 
 export default function UserInformations() {
@@ -21,23 +13,9 @@ export default function UserInformations() {
     const [firstname, setFirstname] = useState('')
     const [name, setName] = useState('')
     const [email, setEmail] = useState('')
-    const [oldPassword, setOldPassword] = useState('')
-    const [password, setPassword] = useState('')
-    const [password2, setPassword2] = useState('')
 
     const [error, setError] = useState('')
     const [modal1Visible, setModal1Visible] = useState(false)
-    const [modal2Visible, setModal2Visible] = useState(false)
-
-    const [oldPasswordVisible, setOldPasswordVisible] = useState(false)
-    const [passwordVisible, setPasswordVisible] = useState(false)
-    const [password2Visible, setPassword2Visible] = useState(false)
-
-
-
-
-
-
 
 
     return (<>
@@ -110,66 +88,6 @@ export default function UserInformations() {
                 maxLength={28}>
             </TextInput>
 
-
-            <Text style={styles.text1}>
-                Change my password :
-            </Text>
-
-            <View style={styles.passwordContainer}>
-                <TextInput style={styles.input2}
-                    onChangeText={(value) => {
-                        setOldPassword(value)
-                        setError('')
-                    }}
-                    value={oldPassword}
-                    placeholder="Former password"
-                    placeholderTextColor="#fbfff790"
-                    secureTextEntry={!oldPasswordVisible}
-                    maxLength={28}>
-                </TextInput>
-                <FontAwesome
-                    name={oldPasswordVisible ? "eye-slash" : "eye"} color="#f9fff4" size={RPH(3.8)} onPress={() => setOldPasswordVisible(!oldPasswordVisible)}>
-                </FontAwesome>
-            </View>
-
-            <View style={styles.passwordContainer}>
-                <TextInput style={styles.input2}
-                    onChangeText={(value) => {
-                        setPassword(value)
-                        setError('')
-                    }}
-                    value={password}
-                    placeholder="New password"
-                    placeholderTextColor="#fbfff790"
-                    secureTextEntry={!passwordVisible}
-                    maxLength={28}>
-                </TextInput>
-                <FontAwesome
-                    name={passwordVisible ? "eye-slash" : "eye"} color="#f9fff4" size={RPH(3.8)} onPress={() => setPasswordVisible(!passwordVisible)}>
-                </FontAwesome>
-            </View>
-
-
-            <View style={styles.passwordContainer}>
-                <TextInput style={styles.input2}
-                    onChangeText={(value) => {
-                        setPassword2(value)
-                        setError('')
-                    }}
-                    value={password2}
-                    placeholder="Confirm new password"
-                    placeholderTextColor="#fbfff790"
-                    secureTextEntry={!password2Visible}
-                    maxLength={28}>
-                </TextInput>
-                <FontAwesome
-                    name={password2Visible ? "eye-slash" : "eye"} color="#f9fff4" size={RPH(3.8)} onPress={() => setPassword2Visible(!password2Visible)}>
-                </FontAwesome>
-            </View>
-
-
-            <Text style={[styles.text1, !error && { display: "none" }, error == "Modifications enregistrées !" ? { color: "green" } : { color: "red" }]}>{error}</Text>
-
             <TouchableOpacity style={styles.btnTouchable} activeOpacity={0.8} onPress={() => setModal1Visible(true)}>
                 <LinearGradient
                     colors={['#9dcb00', '#045400']}
@@ -181,23 +99,6 @@ export default function UserInformations() {
                     <Text style={styles.text2}>Register</Text>
                 </LinearGradient>
             </TouchableOpacity>
-
-            <Text style={[styles.text1, !error && { display: "none" }, { color: "red" }]}>{error}</Text>
-
-
-            <LinearGradient
-                colors={['#9dcb00', '#045400']}
-                locations={[0.05, 1]}
-                start={{ x: 0, y: 0.5 }}
-                end={{ x: 1, y: 0.5 }}
-                style={styles.btnGradientContainer2}
-            >
-                <TouchableOpacity style={[styles.btnTouchable2,]} activeOpacity={0.8} onPress={() => setModal2Visible(true)}>
-                    <Text style={styles.text3}>Unsuscribe</Text>
-                </TouchableOpacity>
-            </LinearGradient>
-
-
 
 
             <Modal
@@ -248,57 +149,6 @@ export default function UserInformations() {
                     </View>
                 </View>
             </Modal>
-
-
-
-            <Modal
-                isVisible={modal2Visible}
-                style={styles.modal}
-                backdropColor="rgba(0,0,0,0.9)"
-                animationIn="slideInDown"
-                animationOut="slideOutUp"
-                statusBarTranslucent={true}
-                onBackButtonPress={() => setModal2Visible(!modal2Visible)}
-                onBackdropPress={() => setModal2Visible(!modal2Visible)}
-            >
-                <View style={styles.modalBody}>
-                    <Text style={styles.text3}>Are you sure ?</Text>
-                    <LinearGradient
-                        colors={['#9dcb00', '#045400']}
-                        locations={[0.05, 1]}
-                        start={{ x: 0, y: 0.5 }}
-                        end={{ x: 1, y: 0.5 }}
-                        style={styles.gradientLine2}
-                    >
-                    </LinearGradient>
-                    <View style={styles.row1}>
-                        <TouchableOpacity style={styles.btnTouchable} activeOpacity={0.8} onPress={() => setModal2Visible(false)}>
-                            <LinearGradient
-                                colors={['#9dcb00', '#045400']}
-                                locations={[0.05, 1]}
-                                start={{ x: 0, y: 0.5 }}
-                                end={{ x: 1, y: 0.5 }}
-                                style={styles.btnGradientContainer}
-                            >
-                                <Text style={styles.text2}>Cancel</Text>
-                            </LinearGradient>
-                        </TouchableOpacity>
-
-                        <TouchableOpacity style={styles.btnTouchable} activeOpacity={0.8} onPress={() => console.log("Unsuscribe")}>
-                            <LinearGradient
-                                colors={['#9dcb00', '#045400']}
-                                locations={[0.05, 1]}
-                                start={{ x: 0, y: 0.5 }}
-                                end={{ x: 1, y: 0.5 }}
-                                style={styles.btnGradientContainer}
-                            >
-                                <Text style={styles.text2}>Confirm</Text>
-                            </LinearGradient>
-                        </TouchableOpacity>
-                    </View>
-                </View>
-            </Modal>
-
 
 
 
@@ -361,26 +211,6 @@ const styles = StyleSheet.create({
         paddingBottom: 7,
         paddingTop: 7,
     },
-    passwordContainer: {
-        flexDirection: "row",
-        width: "100%",
-        height: RPW(10),
-        paddingRight: RPW(2),
-        justifyContent: "space-between",
-        alignItems: "center",
-        backgroundColor: "#2e6017",
-        marginBottom: 20,
-        borderRadius: 5,
-    },
-    input2: {
-        width: "80%",
-        backgroundColor: "#2e6017",
-        borderRadius: 5,
-        paddingLeft: 8,
-        fontSize: RPW(5.3),
-        paddingBottom: 7,
-        paddingTop: 7,
-    },
     btnTouchable: {
         height: RPW(12),
         marginTop: 10,
@@ -393,21 +223,6 @@ const styles = StyleSheet.create({
         paddingLeft: RPW(5),
         paddingRight: RPW(5),
         minWidth: RPW(34)
-    },
-    btnTouchable2: {
-        backgroundColor: "#f9fff4",
-        flex: 1,
-        margin: 2,
-        paddingLeft: RPW(5),
-        paddingRight: RPW(5),
-        borderRadius: 10,
-        alignItems: "center",
-        justifyContent: "center",
-    },
-    btnGradientContainer2: {
-        borderRadius: 10,
-        height: RPW(12),
-        marginTop: 40,
     },
     text2: {
         color: "white",
